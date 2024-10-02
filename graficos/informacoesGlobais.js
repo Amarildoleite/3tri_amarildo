c66f9af95b650c0852343bf47f3e09aecbcc4ecf
Amarildoleite <128834753+Amarildoleite@users.noreply.github.com>
2024-10-02 08:44:59 -0300

    faf

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -23,13 +23,15 @@ async function vizualizarInformacoesGlobais() {
     const dados = await res.json()
     /*Para transformar o número em uma maneira mais fácil de ler*/
     const pessoasConectadas = (dados.total_pessoas_conectadas/1e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo/1e9)    
+    const pessoasNoMundo = (dados.total_pessoas_mundo/1e9) 
+    const horas = parseInt(dados.tempo_medio)  
+    const minutos = Math.round((dados.tempo_medio - horas)*100)
     /*Teremos duas estruturas, uma relacionada só a gráfico e outra relacionada a textos*/
     const paragrafo = document.createElement('p')
     /*IMPORTANTE ESTILIZAR A CLASSE ABAIXO NO CSS*/
     paragrafo.classList.add('graficos-conteiner__texto')
     /*Criar o texto, o símbolo $ serve para colocar entre os textos uma informação. É um tipo de comando dentro do texto*/    
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoas_mundo}bilhões</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoas_mundo}bilhões</span> de pessoas e que aproximadamente <span>${pessoas_conectadas}bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}horas</span> e <span>${minutos} minutos</span> conectadas.`
    
 
     const container = document.getElementById('graficos-container')
