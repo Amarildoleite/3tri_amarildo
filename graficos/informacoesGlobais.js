e295bbcdff0d2442704a310525de2b5eaf139569
Amarildoleite <128834753+Amarildoleite@users.noreply.github.com>
2024-10-09 09:48:45 -0300

    dhgfdh

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -16,7 +16,7 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 
 
 /*IMPORTANTE: NO INDEX LOGO ABAIXO DO FOOTER digite:
-script type="module" src="graficos/informacoesGlobais.js></script>
+<script type="module" src="graficos/informacoesGlobais.js></script>
 */
 
 
@@ -28,9 +28,18 @@ async function vizualizarInformacoesGlobais() {
     const dados = await res.json()
     /*console.log(dados);*/
     /*Teremos duas estruturas, uma relacionada só a gráfico e outra relacionada a textos*/
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas)*100)
+    const porcentagemConectada = ((pessoasConectadas/pessoasNoMundo)*100).toFixed(2)
     const paragrafo = document.createElement('p')
     /*IMPORTANTE ESTILIZAR A CLASSE ABAIXO NO CSS*/
     paragrafo.classList.add('graficos-conteiner__texto')
     /*Criar o texto, o símbolo $ serve para colocar entre os textos uma informação. É um tipo de comando dentro do texto*/    
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
-    console.log(paragrafo)
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos </span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    
+    const container = document.getElementById ('graficos-container')
+    container.appendChild(paragrafo)
+}
+vizualizarInformacoesGlobais()
