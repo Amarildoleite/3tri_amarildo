03b07ad48cb2303b79f04f917a147b0d01035bae
Amarildoleite <128834753+Amarildoleite@users.noreply.github.com>
2024-10-30 10:32:42 -0300

    d

--- graficos/common.js
+++ graficos/common.js
@@ -1,6 +1,6 @@
 /*Para aula de hoje: criar dentro da pasta gráficos, um arquivo chamado: common.js*/
 /*Em seguida cole o código abaixo no arquivo criado*/
-/*acrescentar no arquivo indez, logo apos o fechamento do footer a seguinte linha de comando:
+/*acrescentar no arquivo index, logo após o fechamento do footer a seguinte linha de comando:
 <script type="module" src="graficos/common.js"></script>   */
 
 /*Tudo que for comum, vamos colocar aqui*/
