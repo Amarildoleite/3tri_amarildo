53124c6c41f659ed05288503adc7763c87a01fbf
Amarildoleite <128834753+Amarildoleite@users.noreply.github.com>
2024-10-30 07:47:33 -0300

    d

--- graficos/common.js
+++ graficos/common.js
@@ -1,3 +1,8 @@
+/*Para aula de hoje: criar dentro da pasta gráficos, um arquivo chamado: common.js*/
+/*Em seguida cole o código abaixo no arquivo criado*/
+/*acrescentar no arquivo indez, logo apos o fechamento do footer a seguinte linha de comando:
+<script type="module" src="graficos/common.js"></script>   */
+
 /*Tudo que for comum, vamos colocar aqui*/
 const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
